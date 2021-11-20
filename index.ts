504326439ac31c74481903b5491802caf20b864f
Ben Peachey Higdon <bpeacheyhigdon@gmail.com>
2021-11-19 19:18:25 -0500

    multiline prompt

--- index.ts
+++ index.ts
@@ -5,19 +5,30 @@ export function error(line: number, message: string) {
     console.log("[line " + line + "] Error: " + message)
 }
 
-const prompt = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const runPrompt = () => {
-    prompt.question("JSL> ", (answer) => {
-      if (answer == 'exit') //we need some base case, for recursion
-        return prompt.close(); //closing RL and returning from function.
-      run(answer);
-      runPrompt(); //Calling this function again to ask new question
+function runPrompt() {
+    let text: string[] = [];
+    const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
     });
-};
+
+    rl.on('line', (line) => {
+        if (line.endsWith('\\')) {
+            text.push(line.slice(0, -1));
+        } else {
+            text.push(line)
+            run(text.join('\n'));
+            text = [];
+            rl.prompt();
+        }
+    }).on('close', () => {
+        console.log("\nSession Terminated");
+        process.exit(0);
+    });
+
+    rl.setPrompt('JSL>');
+    rl.prompt();
+}
 
 function run(source: string) {
     const lexer = new Lexer(source);
